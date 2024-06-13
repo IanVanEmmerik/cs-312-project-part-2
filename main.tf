0b6ade891c2034a0ed55ae7e316bed84a6329c39
IanVanEmmerik <vanemmei@oregonstate.edu>
2024-06-12 22:23:00 -0700

    Completed assignment

--- main.tf
+++ main.tf
@@ -1,92 +1,92 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 4.16"
+        }
     }
-  }
 
-  required_version = ">= 1.2.0"
+    required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region = "us-west-2"
+    region  = "us-west-2"
 }
 
 resource "aws_instance" "minecraft2_server" {
-  ami           = "ami-05a6dba9ac2da60cb"
-  instance_type = "t4g.small"
-  key_name      = aws_key_pair.minecraft2.key_name
+    ami           = "ami-05a6dba9ac2da60cb"
+    instance_type = "t4g.small"
+    key_name      = aws_key_pair.minecraft2.key_name
 
-  associate_public_ip_address = true
+    associate_public_ip_address = true
 
-  vpc_security_group_ids = [
-    aws_security_group.minecraft2.id
-  ]
-
-  provisioner "file" {
-    source      = "setup.sh"
-    destination = "/tmp/setup.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/setup.sh",
-      "/tmp/setup.sh",
+    vpc_security_group_ids = [
+        aws_security_group.minecraft2.id
     ]
-  }
 
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = tls_private_key.minecraft2.private_key_pem
-    host        = self.public_ip
-  }
+    provisioner "file" {
+        source      = "setup.sh"
+        destination = "/tmp/setup.sh"
+    }
 
-  tags = {
-    Name = "Minecraft 2"
-  }
+    provisioner "remote-exec" {
+        inline = [
+            "chmod +x /tmp/setup.sh",
+            "/bin/bash /tmp/setup.sh",
+        ]
+    }
+
+    connection {
+        type        = "ssh"
+        user        = "ec2-user"
+        private_key = tls_private_key.minecraft2.private_key_pem
+        host        = self.public_ip
+    }
+
+    tags = {
+        Name = "Minecraft 2"
+    }
 }
 
 resource "aws_key_pair" "minecraft2" {
-  key_name   = "minecraft2"
-  public_key = tls_private_key.minecraft2.public_key_openssh
+    key_name   = "minecraft2"
+    public_key = tls_private_key.minecraft2.public_key_openssh
 }
 
 resource "tls_private_key" "minecraft2" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
+    algorithm = "RSA"
+    rsa_bits  = 4096
 }
 
 resource "aws_security_group" "minecraft2" {
-  name        = "minecraft2-security-group"
-  description = "Allow Minecraft server traffic"
+    name        = "minecraft2-security-group"
+    description = "Allow Minecraft server traffic"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  ingress {
-    from_port   = 25565
-    to_port     = 25565
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    ingress {
+        from_port   = 25565
+        to_port     = 25565
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 resource "local_file" "minecraft2_key" {
-  content  = tls_private_key.minecraft2.private_key_pem
-  filename = "minecraft2.pem"
+    content  = tls_private_key.minecraft2.private_key_pem
+    filename = "minecraft2.pem"
 }
 
 output "minecraft2_server_public_ip" {
